12be4723623f91cf59f19db83902be77992c60f2
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2025-01-07 20:51:21 +0400

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -7,9 +7,9 @@ def personal_sum(numbers):
             result += i
         except TypeError:
             incorrect_data += 1
-            print(f"Некорректный тип данных для подсчёта суммы: {i}")
-    new_tuple = (result, incorrect_data)
-    return new_tuple
+            print(f'Некорректный тип данных для подсчёта суммы - {i}')
+    total = (result, incorrect_data)
+    return total
 
 
 def calculate_average(numbers):
@@ -20,24 +20,24 @@ def calculate_average(numbers):
         if not numbers:
             return 0
 
-        sum_of_numbers, incorrect_data = personal_sum(numbers)
+        result, incorrect_data = personal_sum(numbers)
 
-        if sum_of_numbers is None or len(numbers) - incorrect_data == 0:
+        if result is None or len(numbers) - incorrect_data == 0:
             return 0
 
-        average = sum_of_numbers / (len(numbers) - incorrect_data)
+        arithmetic_mean = result / (len(numbers) - incorrect_data)
 
-        return average
-    except ZeroDivisionError as exc:
-        print(exc)
+        return arithmetic_mean
+
+    except ZeroDivisionError as e:
+        print(e)
         return 0
-    except TypeError as exc:
-        print(exc)
-        return None
+    except TypeError as e:
+        print(e)
+    return None
 
 
-if __name__ == "__main__":
-    print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
-    print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
-    print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
-    print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
